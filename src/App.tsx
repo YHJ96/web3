5611952ea4467d601631f18574487cc191b611ad
YHJ96 <yhj878812@naver.com>
2022-12-19 15:11:37 +0900

    feat: 네트워크 교체 로직 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,72 @@
-import { useEffect } from "react";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 
+/**
+ * 1. 최초의 지갑을 연결하여 chainId를 확인한다.
+ * 2. 97번 바이낸스 메인넷인지 확인한다.
+ * 3. 바이낸스 메인넷이 아니라면 네트워크를 바꾸는 창을 출력한다.
+ */
+
 type Account = string | null | undefined;
 type Library = Web3Provider | undefined;
+type ChainId = number | undefined;
 
-const injected = new InjectedConnector({});
+interface Ethereum extends Window {
+  ethereum: {
+    request(args: RequestArguments): Promise<unknown>;
+  };
+}
+
+interface RequestArguments {
+  method: string;
+  params?: unknown[] | object;
+}
 
 function App() {
+  const ethereum = (window as unknown as Ethereum).ethereum;
   const { chainId, account, active, activate, deactivate, library } =
     useWeb3React<Web3Provider>();
 
-  const handleConnect = () => {
-    activate(injected, (err) => {
-      const error = err as Error;
-      if (error) {
-        throw error.message;
-      }
-    });
+  const connectWallet = async () => {
+    const initInjected = new InjectedConnector({});
+    await activate(initInjected);
+
+    if (isCheckedChainId(chainId)) {
+      await getData(account, library);
+    } else {
+      changeChainId();
+    }
   };
 
-  const handleDisConnect = () => {
+  const disConnectWallet = () => {
     if (active) {
       return deactivate();
     }
   };
 
-  const getData = async (account: Account, library: Library) => {
-    if (account) {
-      const result = await library?.getBalance(account);
-      console.log(result);
+  const isCheckedChainId = (chainId: ChainId) => {
+    if (chainId === 97 && active) {
+      return true;
+    } else {
+      return false;
     }
   };
 
-  useEffect(() => {
-    getData(account, library);
-  }, [account, library]);
+  const changeChainId = () => {
+    ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x61" }],
+    });
+  };
+
+  const handleOnClick = (callback: (...args: any) => any) => () => callback();
+
+  const getData = async (account: Account, library: Library) => {
+    if (account) {
+      await library?.getBalance(account);
+    }
+  };
 
   return (
     <div>
@@ -45,10 +75,10 @@ function App() {
         <p>네트워크 아이디 {chainId}</p>
         <p>지갑 주소 {account}</p>
       </div>
-      <button type="button" onClick={handleConnect}>
+      <button type="button" onClick={handleOnClick(connectWallet)}>
         메타 마스크 연결
       </button>
-      <button type="button" onClick={handleDisConnect}>
+      <button type="button" onClick={handleOnClick(disConnectWallet)}>
         메타 마스크 연결해제
       </button>
     </div>
